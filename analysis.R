5e54d9c03c25fa0e649dae2702e7b286eb1a390c
rli1010 <38045324+rli1010@users.noreply.github.com>
2022-06-15 10:46:12 -0500

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -1,7 +1,28 @@
 library(Rcpp) 
 library(truncnorm)
 
-#simda=read.csv("simdata.csv")
-#source("functions/utils.R")
+simda<- read.csv("simdata.csv",header=TRUE)
+sourceCpp("code.cpp")
+source("functions.R")
 
-trans.reg(cbind(Y1,Y2,Y3,Y4) ~ Z1+Z2+Z3+Z4, data = simda)
+##Fitting start here
+##Data rows with missing data need to be removed before fitting
+
+#Transformed linear model
+ff=trans.reg(cbind(Y1,Y2,Y3,Y4) ~ Z1+Z2+Z3+Z4, data = simda)
+ff
+
+#Standardized to unit norm
+ff$Coef[-1]/sqrt(sum(ff$Coef[-1]^2))
+
+
+#MRC
+#Set resamp to an integer>=200 for real application
+
+#As the sign of beta[1] is fixed to be positive, set the first variable in varlist as a covariate with positive coef (e.g.,  a significant variable with positive 
+#coef in the transformed linear model)
+
+mono.fit=mono.reg(Ylist=c("Y1","Y2","Y3","Y4"),varlist=c("Z1","Z2","Z3","Z4"),
+                  da=simda,n.init=10,resamp=30)  
+
+mono.fit$est
