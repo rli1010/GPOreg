d0f85a360e4c08904ca0b76272cb41ce5f94a36c
rli1010 <38045324+rli1010@users.noreply.github.com>
2022-06-15 10:48:49 -0500

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -22,7 +22,5 @@ ff$Coef[-1]/sqrt(sum(ff$Coef[-1]^2))
 #As the sign of beta[1] is fixed to be positive, set the first variable in varlist as a covariate with positive coef (e.g.,  a significant variable with positive 
 #coef in the transformed linear model)
 
-mono.fit=mono.reg(Ylist=c("Y1","Y2","Y3","Y4"),varlist=c("Z1","Z2","Z3","Z4"),
-                  da=simda,resamp=30)  
-
+mono.fit=mono.reg(Ylist=c("Y1","Y2","Y3","Y4"),varlist=c("Z1","Z2","Z3","Z4"),da=simda,resamp=30)  
 mono.fit$est
